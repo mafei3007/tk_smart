7aeae43b6cfc6cc5956ec49c078684d696e61f54
mafei3007 <13951623007@163.com>
2022-04-01 20:49:57 +0800

    公共变量修改

--- src/constant.py
+++ src/constant.py
@@ -12,18 +12,35 @@ comm_db_user = 'root'  # 账户名
 comm_db_pwd = '123'  # 密码
 comm_db_host = '127.0.0.1'  # DB服务器IP
 comm_db_charset = 'utf8'  # 字符集
+
+# 单位类型枚举
 comm_unit_type = ['数量', '重量', '容量', '面积', '长度', '自定义']
+
+# 系统默认角色，不能修改
 comm_role = ['超级用户', '仓库管理', '订单下发', '订单审核', '财务', '生产', '销售', '采购']
+
 company_name = 'company_name'
+
 company_bank = 'company_bank'
+
 company_account = 'company_account'
+
 company_credit_code = 'company_credit_code'
+
 company_address = 'company_address'
+
 pc_idx = 'pc_idx'  # 采购订单编号前缀
+
 so_idx = 'so_idx'  # 销售订单编号前缀
+
 prod_idx = 'prod_idx'  # 生产工单名称前缀
+
 account_delay_day = 'account_delay_day'  # 账期宽限天数
+
 contract_validity = 'contract_validity'  # 合同有效期（年)
+
 free_maintenance_year = 'free_maintenance_year'  # 免费维保年限
+
 maintenance_year = 'maintenance_year'  # 维保年限
-default_pwd='123'
+
+default_pwd = '123'
