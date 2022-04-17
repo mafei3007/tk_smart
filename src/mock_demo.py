e9af3f6c3f1010614a39428e0a25ab582dce99a7
mafei3007 <13951623007@163.com>
2022-04-17 16:20:28 +0800

    添加了功能测试脚本

--- src/mock_demo.py
+++ src/mock_demo.py
@@ -17,8 +17,9 @@ from process.tk_em import add_em
 g_th_queue = Queue()
 
 
-def process_add_em(prefix_name, prefix_code):
-    for i in range(1, 1000):
+def process_add_em(prefix_name, prefix_code, n_count):
+    t_start = time.time()
+    for i in range(1, n_count):
         name = '卡尔.%s_%d' % (prefix_name, i)
         code = '卡尔.%s_%d' % (prefix_code, i)
         js = {'tenant': 'tk_huawei', 'opt_id': 1, 'name': name, 'code': code, 'duty': '掌门',
@@ -26,21 +27,27 @@ def process_add_em(prefix_name, prefix_code):
               'addr': '武当山'}
         add_em(js)
     g_th_queue.get_nowait()
-    print(prefix_name, prefix_code, '完成了')
+    avg_value = 1000 * (time.time() - t_start) / n_count
+    str_msg = '姓名前缀%10s,代号前缀%10s,共%d条记录,每条记录耗时%5.2f毫秒' % (prefix_name, prefix_code, n_count, avg_value)
+    print(str_msg)
 
 
 def main():
-    for i in range(1, 20):
+    th_count = 20
+    m_count = 100
+    m_start = time.time()
+    for i in range(1, th_count):
         g_th_queue.put_nowait(i)
         prefix_name = '姓名_%d' % i
         prefix_code = 'code_%d' % i
-        _thread.start_new_thread(process_add_em, (prefix_name, prefix_code))
-        print('==========', i)
+        _thread.start_new_thread(process_add_em, (prefix_name, prefix_code, m_count))
+    while g_th_queue.qsize() > 0:
+        time.sleep(1)
+    elapsed_ms = (time.time() - m_start)
+    m_msg = '所有数据已经预置完成,耗时%.2f秒' % elapsed_ms
+    print(m_msg)
 
 
 if __name__ == '__main__':
-    t1 = time.time()
+    # 这是一个样例
     main()
-    while g_th_queue.qsize() > 0:
-        time.sleep(10)
-    print('所有数据已经预置完成', time.time() - t1)
